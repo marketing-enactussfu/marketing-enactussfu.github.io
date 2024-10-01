2a76f9b30047cbfb600199cabc60dbed739728d9
yanjunq <mandyqian58@gmail.com>
2024-10-01 02:35:58 -0700

    fix

--- frontend/src/CurrentProjects/TurbioEnergy.jsx
+++ frontend/src/CurrentProjects/TurbioEnergy.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import PageTemplate from '../components/PageTemplate';
 import turbio from '../images/currentprojects/turbio.png'; 
+import CurrentProjectsTemplate from "../components/CurrentProjectsTemplate";
+
 
 const Turbio = () => {
-    
-    return <CurrentProjectsTemplate title="Turbio Energy" text="We aim to repurpose cooking oil to reduce environmental pollution, lower carbon emissions, and promote sustainable energy practices." pic={deskpic} />;
+
+    return <CurrentProjectsTemplate title="Turbio Energy" text="We aim to repurpose cooking oil to reduce environmental pollution, lower carbon emissions, and promote sustainable energy practices." pic={turbio} />;
 };
 
 export default Turbio;
