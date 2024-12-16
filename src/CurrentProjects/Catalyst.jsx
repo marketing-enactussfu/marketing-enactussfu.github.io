903abde5a64e4d724a4248dfbda3afcaeefc66bb
marketing-enactussfu <media@enactussfu.com>
2024-12-15 23:42:01 -0800

    Update Catalyst.jsx

--- src/CurrentProjects/Catalyst.jsx
+++ src/CurrentProjects/Catalyst.jsx
@@ -4,7 +4,7 @@ import ecpic from '../images/currentprojects/challenges.png';
 import CurrentProjectsTemplate from "../components/CurrentProjectsTemplate";
 
 const Catalyst = () => {
-    const text = 'A 8-10 week collaborative program with the Charles Chang Institute of Catalyst to enable students to unleash their catalyst mindset.;'
+    const text = 'In collaboration with the Charles Chang Institute for Entrepreneurship, Catalyst is an 8 week program providing an introduction to entrepreneurship for SFU students through development of innovative solutions to challenges posed by industry partners.'
     return <CurrentProjectsTemplate title="Catalyst" text={text} pic={ecpic} />;  
 
 };
